b26f4c3cc65b573608d44b27f17e4428d83b7c8a
mavisakalyan <55106546+mavisakalyan@users.noreply.github.com>
2025-08-29 19:48:45 +0400

    Fix route matching

--- backend/src/modules/classes/classes-router.js
+++ backend/src/modules/classes/classes-router.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const classesController = require("./classes-controller");
 const { checkApiAccess } = require("../../middlewares");
 
-router.get("", checkApiAccess, classesController.handleFetchAllClasses);
+router.get("/", checkApiAccess, classesController.handleFetchAllClasses);
 router.get("/:id", checkApiAccess, classesController.handleFetchClassDetail);
-router.post("", checkApiAccess, classesController.handleAddClass);
+router.post("/", checkApiAccess, classesController.handleAddClass);
 router.put("/:id", checkApiAccess, classesController.handleUpdateClass);
 router.delete("/:id", checkApiAccess, classesController.handleDeleteClass);
 
